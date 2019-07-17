dea508bd71ce89dae5d94ccda61fa09098910ec8
maryam032 <maryamakram032@gmail.com>
2019-07-17 05:12:59 +0500

    Login form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import Header from '../src/Components/Header.js';
 import Body from '../src/Components/Body'
+
 
 class App extends Component {
   render()
   {
     return (
     <div>
-      <Header/>
+    
       <Body/>
     </div>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,11 @@
 import React from 'react'
+import { inherits } from 'util';
 
 class Header extends React.Component{
     render()
     {
         return(
-        <div>
-        <h1 style={{textAlign:'center',margin:30,color:'white',backgroundColor:'#2C3E50',fontWeight:'bold',padding:20,borderRadius:20}}>Sign In</h1>
-        
-        </div>
+            <h1>Login Page</h1>
         )
     }
 }
